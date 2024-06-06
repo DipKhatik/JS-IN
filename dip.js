54ca6ce8952e248655bddc862ce5bacec44e9313
deep khatik <deepkhatik31122003@gmail.com>
2024-06-07 01:41:38 +0530

    dataTypes

---
+++ dip.js
@@ -0,0 +1,12 @@
+let accountName ="hitesh"; 
+const accountId = 123;
+var   emailId = "hitesh@123";
+city = "pune";  
+
+accountName = "dip";
+emailId = "dip@123";
+city = "delhi";
+
+console.table([accountName,accountId,emailId,city]);
+// accountId = 456;
+// console.log(accountId);
